7eaad15d681f346adab5734d7eb444fbd9cff931
Christossofos <Christossofos@hotmail.com>
2022-03-14 20:37:58 +0200

    14/03/22
    JavaScript interactive quiz

---
+++ Quiz/app.js
@@ -0,0 +1,34 @@
+const correctAnswers = ["A", "B", "A", "B", "B"];
+const form = document.querySelector(".quiz-form");
+const result = document.querySelector(".result");
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+
+    let score = 0;
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value];
+
+    // check answers
+    userAnswers.forEach((answer, index) => {
+        if(answer === correctAnswers[index]){
+            score += 20;
+        }
+    });
+
+    scrollTo(0, 0);
+    result.classList.remove("d-none");
+    //console.log(score);
+
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector("span").textContent = `${output}%`;
+        if(output === score)
+            clearInterval(timer);
+        else
+            output++;
+    }, 10);
+});
+
+setTimeout(() => {
+    alert("Thank you for playing!");
+}, 2000); // in millinsecs
